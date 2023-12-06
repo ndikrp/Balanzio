26581e15524513f4031d1e3ba092046e301add39
DeaAyuninda05 <nindadea05@gmail.com>
2023-12-06 18:54:49 +0700

    commit 002

--- app/src/main/java/com/dicoding/balanzio/MainActivity.kt
+++ app/src/main/java/com/dicoding/balanzio/MainActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.dicoding.balanzio.databinding.ActivityMainBinding
+import com.dicoding.balanzio.ui.fragment.GoalsFragment
+import com.dicoding.balanzio.ui.fragment.HistoryFragment
+import com.dicoding.balanzio.ui.fragment.HomeFragment
+import com.dicoding.balanzio.ui.fragment.ResepFragment
 import com.dicoding.balanzio.ui.splash.SplashscreenActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +24,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        replaceFragment(HomeFragment())
+
 //        viewModel.getSession().observe(this) { user ->
 //            if (!user.isLogin) {
 //                startActivity(Intent(this, SplashscreenActivity::class.java))
 //                finish()
 //            }
 //        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.history -> replaceFragment(HistoryFragment())
+                R.id.resep -> replaceFragment(ResepFragment())
+                R.id.goals -> replaceFragment(GoalsFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
