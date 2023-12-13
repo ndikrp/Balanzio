1c49558a1118629095a9624138a7c57cc897d28f
Andhika Rizky <90492415+ndikrp@users.noreply.github.com>
2023-12-13 14:38:09 +0700

    Update food index.js

--- routes/food/index.js
+++ routes/food/index.js
@@ -14,4 +14,18 @@ router.get('/foods', (req, res) => {
     });
 });
 
+router.get('/foods/:category', (req, res) => {
+    const category = req.params.category;
+  
+    const selectFoodsSql = 'SELECT * FROM food WHERE category = ?'; 
+  
+    pool.query(selectFoodsSql, [category], (error, results) => {
+      if (error) {
+        console.error('Error querying foods:', error);
+        return res.status(500).json({ error: 'Error querying foods' });
+      }
+      res.json({ status: 'Success', food: results });
+    });
+  });
+
 module.exports = router;
