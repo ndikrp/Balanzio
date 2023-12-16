132a09a469087682741315d184757ce1b089021d
DeaAyuninda05 <nindadea05@gmail.com>
2023-12-16 09:41:16 +0700

    commit 008 (update UI)

---
+++ app/src/main/java/com/dicoding/balanzio/ui/activity/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.balanzio.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.balanzio.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
